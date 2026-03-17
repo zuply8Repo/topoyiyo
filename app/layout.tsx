a1bac8179fae9de7eae7b904f8a130286168403d
zuply8Repo <mplata@zuply8.com>
2026-03-17 14:03:05 +0100

    Login Theme change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { shadcn } from "@clerk/ui/themes";
 import "./globals.css";
 import Providers from "./providers";
 
@@ -80,7 +81,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
+        <ClerkProvider
+          appearance={{
+            theme: shadcn,
+          }}
+        >
           <Providers>{children}</Providers>
         </ClerkProvider>
       </body>
